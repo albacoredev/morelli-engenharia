484daef2a1e18442ba82f10bcdaf45b6e3d08056
Gabriel Andrade <gabriel.andrade@outlook.com.br>
2023-04-07 16:51:00 -0300

    feat: setup IMask

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import suite from '$lib/vestSuites/suite';
+	import mask from '$lib/mask';
 
 	console.log(
 		suite({
@@ -10,3 +11,4 @@
 
 <h1 class="text-3xl font-bold underline">Hello world!</h1>
 <button class="btn btn-primary">Button</button>
+<input type="text" use:mask={'00.000.000/0000-00'} />
