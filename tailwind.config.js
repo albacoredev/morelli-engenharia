4220f75e8ff150fb9f73178b8c30fe899f3b2f16
Gabriel Andrade <gabriel.andrade@outlook.com.br>
2023-04-07 16:07:55 -0300

    feat: setup DaisyUI theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,22 @@ export default {
 	theme: {
 		extend: {}
 	},
+	daisyui: {
+		themes: [
+			{
+				morelli: {
+					primary: '#13A89E',
+					secondary: '#3A2319',
+					accent: '#C96C00',
+					neutral: '#222D3A',
+					'base-100': '#d6d3d1',
+					info: '#81B6D5',
+					success: '#19A982',
+					warning: '#EECD59',
+					error: '#F86D7F'
+				}
+			}
+		]
+	},
 	plugins: [require('daisyui')]
 };
