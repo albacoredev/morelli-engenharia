265104dee9ce1bca6a160996070eb74ce3176161
Gabriel Andrade <gabriel.andrade@outlook.com.br>
2023-04-07 21:30:09 -0300

    feat: add radio input component

--- src/lib/vestSuites/suite.ts
+++ src/lib/vestSuites/suite.ts
@@ -3,8 +3,12 @@ import { create, test, enforce, only } from 'vest';
 const suite = create((data = {}, current?: string) => {
 	current && only(current);
 
-	test('cnpj', 'Username is required', () => {
+	test('cnpj', 'CNPJ is required', () => {
 		enforce(data.cnpj).isNotBlank();
+	});
+
+	test('radio', 'Radio is required', () => {
+		enforce(data.radio).isNotBlank();
 	});
 });
 
