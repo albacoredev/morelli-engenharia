ee34e0486889a1551893e77a1f7811e86f3dd207
Gabriel Andrade <gabriel.andrade@outlook.com.br>
2023-04-12 12:00:17 -0300

    feat: make the label and value the same on radio

--- src/lib/components/Radio.svelte
+++ src/lib/components/Radio.svelte
@@ -1,12 +1,7 @@
 <script lang="ts">
 	import type { SuiteResult } from 'vest';
 
-	interface RadioProps {
-		value: string;
-		label: string;
-	}
-
-	export let options: RadioProps[];
+	export let options: string[];
 	export let result: SuiteResult;
 	export let validate: (name: string) => void;
 	export let name: string;
@@ -20,18 +15,20 @@
 		<span class="label label-text-alt text-error">{result.getErrors()[name][0]}</span>
 	{/if}
 </div>
-{#each options as option}
-	<div class="form-control">
-		<label class="label cursor-pointer">
-			<span class="label-text">{option.label}</span>
-			<input
-				type="radio"
-				{name}
-				class="radio radio-primary"
-				bind:group={selected}
-				value={option.value}
-				on:change|self={() => validate(name)}
-			/>
-		</label>
-	</div>
-{/each}
+<div class="flex flex-col gap-2">
+	{#each options as option}
+		<div class="form-control">
+			<label class="flex items-center gap-2 cursor-pointer">
+				<input
+					type="radio"
+					{name}
+					class="radio radio-primary"
+					bind:group={selected}
+					value={option}
+					on:change|self={() => validate(name)}
+				/>
+				<span class="label-text">{option}</span>
+			</label>
+		</div>
+	{/each}
+</div>
