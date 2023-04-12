a48e5ae6c9427edf14e14908a3b7d43504102be7
Gabriel Andrade <gabriel.andrade@outlook.com.br>
2023-04-12 11:59:29 -0300

    feat: add textarea variant to input

--- src/lib/components/Input.svelte
+++ src/lib/components/Input.svelte
@@ -8,18 +8,29 @@
 	export let name = '';
 	export let result: SuiteResult;
 	export let validate: (name: string) => void;
+	export let type: 'text' | 'textArea' = 'text';
 </script>
 
 <div class="form-control w-full max-w-xs">
-	<input
-		type="text"
-		{name}
-		{placeholder}
-		use:masked={mask}
-		class="input input-bordered input-primary w-full max-w-xs"
-		bind:value
-		on:input={() => validate(name)}
-	/>
+	{#if type === 'textArea'}
+		<textarea
+			{placeholder}
+			{name}
+			class="textarea textarea-bordered textarea-primary w-full max-w-xs"
+			bind:value
+			on:input={() => validate(name)}
+		/>
+	{:else if type === 'text'}
+		<input
+			type="text"
+			{name}
+			{placeholder}
+			use:masked={mask}
+			class="input input-bordered input-primary w-full max-w-xs"
+			bind:value
+			on:input={() => validate(name)}
+		/>
+	{/if}
 	{#if result.getErrors()[name]}
 		<label class="label" for={placeholder}>
 			<span class="label-text-alt text-error">{result.getErrors()[name][0]}</span>
