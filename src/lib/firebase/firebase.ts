df749c7ba048ecbb440c87a4e23510d4b9124148
Gabriel Andrade <gabriel.andrade@outlook.com.br>
2023-04-26 16:59:15 -0300

    feat: setup firebase emulators

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
-import { getFirestore } from '@firebase/firestore';
+import { connectFirestoreEmulator, getFirestore } from '@firebase/firestore';
+import { connectAuthEmulator, getAuth } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,5 +22,9 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
 
-export { db, analytics };
+connectAuthEmulator(auth, 'http://localhost:9099');
+connectFirestoreEmulator(db, 'localhost', 8080);
+
+export { db, analytics, auth };
