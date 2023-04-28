9256541ed8f170a1527765750f484af34662b026
Gabriel Andrade <gabriel.andrade@outlook.com.br>
2023-04-28 11:38:22 -0300

    feat: add navbar to heat form page

--- src/routes/calor/+page.svelte
+++ src/routes/calor/+page.svelte
@@ -50,27 +50,33 @@
 </script>
 
 <div class="flex items-center flex-col">
-	<div class="navbar bg-base-300">
+	<div class="navbar bg-base-100">
 		<div class="flex-none">
-			<button class="btn btn-square btn-ghost">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					class="inline-block w-5 h-5 stroke-current"
-					><path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M4 6h16M4 12h16M4 18h16"
-					/></svg
-				>
-			</button>
+			<a href="./home">
+				<button class="btn btn-square btn-ghost">
+					<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
+						><g id="SVGRepo_bgCarrier" stroke-width="0" /><g
+							id="SVGRepo_tracerCarrier"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						/><g id="SVGRepo_iconCarrier">
+							<g id="Arrow / Caret_Left_SM">
+								<path
+									id="Vector"
+									d="M13 15L10 12L13 9"
+									stroke="#000000"
+									stroke-width="2"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								/>
+							</g>
+						</g></svg
+					>
+				</button>
+			</a>
 		</div>
 		<div class="flex-1">
-			<span class="text-base-content font-bold normal-case text-xl"
-				>Avaliação Quantitativa de Calor</span
-			>
+			<span class="normal-case text-xl px-4">daisyUI</span>
 		</div>
 	</div>
 
