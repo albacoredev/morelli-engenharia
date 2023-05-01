82a62e7069822bf6e4fbd6abf94d13bbbf4d2584
Gabriel Andrade <gabriel.andrade@outlook.com.br>
2023-04-30 22:07:38 -0300

    feat: add a layout to redirect upon signout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,18 @@
-<script>
+<script lang="ts">
 	import '../app.css';
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+
+	onMount(async () => {
+		const { auth } = await import('$lib/firebase/firebase');
+
+		auth.onAuthStateChanged((user) => {
+			if (!user) {
+				goto('/');
+			}
+		});
+	});
 </script>
 
+<button class="btn" on:click={signOut}>Signout</button>
 <slot />
