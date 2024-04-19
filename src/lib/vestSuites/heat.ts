d93fa57d88926308d00872e9c110b3182bb1b44a
Gabriel Andrade <gabriel.andrade@outlook.com.br>
2024-04-19 12:30:28 -0300

    feat: only name and date required

--- src/lib/vestSuites/heat.ts
+++ src/lib/vestSuites/heat.ts
@@ -1,5 +1,4 @@
 import type { IHeatForm } from '$lib/interfaces/forms/heat';
-import hmsToSeconds from '$lib/utils/hmsToSeconds';
 import { create, test, enforce } from 'vest';
 
 const notRequiredFields = ['temperature', 'humidity', 'wind', 'type', 'signatures'];
@@ -11,18 +10,6 @@ const suite = create((data: IHeatForm) => {
 		test(field, 'Campo obrigatório', () => {
 			enforce(data[field as keyof typeof data]).isNotEmpty();
 		});
-	});
-
-	test('startingTime', 'Horário inválido', () => {
-		enforce(data.startingTime).matches(/^([0-1]\d|2[0-3]):([0-5]\d):([0-5]\d)$/);
-	});
-
-	test('endingTime', 'Horário inválido', () => {
-		enforce(data.endingTime).matches(/^([0-1]\d|2[0-3]):([0-5]\d):([0-5]\d)$/);
-	});
-
-	test('endingTime', 'Horário menor que o inicial', () => {
-		enforce(hmsToSeconds(data.endingTime)).greaterThanOrEquals(hmsToSeconds(data.startingTime));
 	});
 });
 
--- src/lib/vestSuites/noise.ts
+++ src/lib/vestSuites/noise.ts
@@ -1,5 +1,4 @@
 import type { INoiseForm } from '$lib/interfaces/forms/noise';
-import hmsToSeconds from '$lib/utils/hmsToSeconds';
 import { create, test, enforce } from 'vest';
 
 const suite = create((data: INoiseForm) => {
@@ -7,18 +6,6 @@ const suite = create((data: INoiseForm) => {
 		test(field, 'Campo obrigatório', () => {
 			enforce(data[field as keyof typeof data]).isNotEmpty();
 		});
-	});
-
-	test('startingTime', 'Horário inválido', () => {
-		enforce(data.startingTime).matches(/^([0-1]\d|2[0-3]):([0-5]\d):([0-5]\d)$/);
-	});
-
-	test('endingTime', 'Horário inválido', () => {
-		enforce(data.endingTime).matches(/^([0-1]\d|2[0-3]):([0-5]\d):([0-5]\d)$/);
-	});
-
-	test('endingTime', 'Horário menor que o inicial', () => {
-		enforce(hmsToSeconds(data.endingTime)).greaterThanOrEquals(hmsToSeconds(data.startingTime));
 	});
 });
 
